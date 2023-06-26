c31f1170b872720c8a9105c978deeafd2e37c5f9
Minsoo Choo <minsoochoo0122@proton.me>
2023-06-26 01:37:42 -0400

    Set Swift package deployment target to iOS 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RQShineLabel",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
